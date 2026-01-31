be2cde1f5fd3d02cc5b5c3714da6eb155affe3aa
BennXYZ <benny.zoeller@gmail.com>
2026-01-31 20:38:06 +0100

    keep working on animations

--- GGJ2026/Assets/Game/Animatable.cs
+++ GGJ2026/Assets/Game/Animatable.cs
@@ -1,18 +1,50 @@
 using System;
 using System.Collections;
+using UnityEditor;
 using UnityEngine;
 
 public class Animatable : MonoBehaviour
 {
     [SerializeField] private Animator animator;
 
+    private AnimationEvent[] currentEvents;
+    private Action onEndEvent;
+    private float previousNormalizedProgress = 0;
+
     private void Reset()
     {
         animator = GetComponent<Animator>();
     }
 
+    private void Update()
+    {
+        if (currentEvents != null)
+        {
+            foreach (AnimationEvent actionEvent in currentEvents)
+            {
+                actionEvent.Update(Time.deltaTime);
+            }
+
+            var currentAnimatorState = animator.GetCurrentAnimatorStateInfo(0);
+            if (currentAnimatorState.normalizedTime >= 1 ||
+                previousNormalizedProgress > currentAnimatorState.normalizedTime)
+            {
+                onEndEvent?.Invoke();
+                foreach (AnimationEvent actionEvent in currentEvents)
+                {
+                    actionEvent.Reset();
+                }
+            }
+            else
+            {
+                previousNormalizedProgress = currentAnimatorState.normalizedTime;
+            }
+        }
+    }
+    
     public void PlayAnimation(string state, int delay = 0, Action onEnd = null, params AnimationEvent[] events)
     {
+        previousNormalizedProgress = 0;
         StopAllCoroutines();
         if (delay > 0)
         {
@@ -32,19 +64,11 @@ public class Animatable : MonoBehaviour
     private IEnumerator DoPlayAnimation(string state, Action onEnd, AnimationEvent[] events)
     {
         animator.Play(state);
+        currentEvents = null;
+        onEndEvent = null;
         yield return null; // animators don't update their "current state" immediately. Wait 1 Frame to ensure we get the correct clip length;
-        int clipLength = Mathf.RoundToInt(animator.GetCurrentAnimatorStateInfo(0).length) * 30 - 1;
-
-        foreach (AnimationEvent actionEvent in events)
-        {
-            actionEvent.callTime = actionEvent.callTime - 1;
-            StartCoroutine(PrepareEvent(actionEvent, Mathf.RoundToInt(clipLength)));
-        }
-
-        if (onEnd != null)
-        {
-            StartCoroutine(PrepareEvent(new AnimationEvent(onEnd, clipLength), clipLength));
-        }
+        onEndEvent = onEnd;
+        currentEvents = events;
     }
 
     private IEnumerator PrepareEvent(AnimationEvent actionEvent, int clipLength)
@@ -59,11 +83,31 @@ public class Animatable : MonoBehaviour
     {
         public Action action;
         public int callTime;
+        private bool triggered;
+        private float timer;
 
         public AnimationEvent(Action action, int callTime)
         {
             this.action = action;
             this.callTime = callTime;
+        }
+
+        public void Reset()
+        {
+            triggered = false;
+            timer = 0;
+        }
+
+        public void Update(float deltaTime)
+        {
+            if (triggered)
+                return;
+            timer += deltaTime;
+            if (timer * 30 > callTime)
+            {
+                action?.Invoke();
+                triggered = true;
+            }
         }
     }
 }
