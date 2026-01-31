34013d8f57321d04b40dd31cf30b881decbcef20
BennXYZ <benny.zoeller@gmail.com>
2026-01-31 14:32:32 +0100

    Start implementing UI

---
+++ GGJ2026/Assets/Game/UI/PlayerBar.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUi : MonoBehaviour
+{
+    [SerializeField] private Image maskRenderer;
+    [SerializeField] private RectTransform airRenderer;
+    [SerializeField] private RectTransform backgroundRenderer;
+
+    [SerializeField] private List<PlayerMaskThreshold> healthMasks;
+    
+    private int airBarSpriteWidth;
+    private int minimumBarWidth;
+    private float airRightSideOffset;
+    private float currentMaxAir;
+    private float currentAir;
+
+    private void Awake()
+    {
+        airRightSideOffset = airRenderer.offsetMax.x;
+        airBarSpriteWidth = Mathf.RoundToInt(airRenderer.GetComponent<Image>().sprite.rect.width);
+        minimumBarWidth = Mathf.RoundToInt(backgroundRenderer.rect.width - airRenderer.rect.width) - 1;
+        healthMasks.Sort((a,b) => a.minimumHealth.CompareTo(b.minimumHealth));
+    }
+
+    private void Start()
+    {
+        MaxPlayerAirSet(10);
+        PlayerAirSet(6);
+    }
+
+    public void PlayerHealthSet(float health)
+    {
+        int index = 0;
+        for (int i = 0; i < healthMasks.Count; i++)
+        {
+            PlayerMaskThreshold threshold = healthMasks[i];
+            if (health < threshold.minimumHealth)
+            {
+                break;
+            }
+            index = i;
+        }
+    }
+
+    public void PlayerAirSet(float air)
+    {
+        if (!airRenderer)
+            return;
+
+        currentAir = air;
+        UpdateAir();
+    }
+
+    public void MaxPlayerAirSet(float maxAir)
+    {
+        if (!backgroundRenderer)
+            return;
+
+        currentMaxAir = maxAir;
+        backgroundRenderer.sizeDelta = new Vector2(minimumBarWidth + maxAir * airBarSpriteWidth, backgroundRenderer.sizeDelta.y);
+        UpdateAir();
+    }
+
+    private void UpdateAir()
+    {
+        airRenderer.offsetMax = new Vector2(airRightSideOffset - (currentMaxAir - currentAir) * airBarSpriteWidth, airRenderer.offsetMax.y);
+    }
+
+    [System.Serializable]
+    public class PlayerMaskThreshold
+    {
+        [SerializeField] public float minimumHealth;
+        [SerializeField] public Sprite sprite;
+    }
+}
