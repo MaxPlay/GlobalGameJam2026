04f74b1220f2750553596427dec7f6530fd75cbc
Max <maxplayyt@gmail.com>
2026-01-31 12:45:43 +0100

    Added character controller (used the Unity one, because it actually works fine) Added shader for slopes

--- GGJ2026/Assets/Game/Player/PlayerController.cs
+++ GGJ2026/Assets/Game/Player/PlayerController.cs
@@ -7,28 +7,39 @@ public class PlayerController : MonoBehaviour
 {
     private InputAction moveAction;
     private InputAction jumpAction;
-
+    private CharacterController characterController;
     [SerializeField]
     private Transform sprite;
-
-    private new Rigidbody rigidbody;
+    [SerializeField]
+    private float speed = 1;
 
     private void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         jumpAction = InputSystem.actions.FindAction("Jump");
-
-        rigidbody = GetComponent<Rigidbody>();
+        characterController = GetComponent<CharacterController>();
     }
 
     private void Update()
     {
-        Vector2 moveValue = moveAction.ReadValue<Vector2>();
-        rigidbody.AddForce(new Vector3(moveValue.x, 0, moveValue.y), ForceMode.Force);
+        Vector3 position = transform.position;
+        Vector2 moveValue = moveAction.ReadValue<Vector2>() * speed;
+
+        characterController.SimpleMove(new Vector3(moveValue.x, 0, moveValue.y));
 
         if (jumpAction.IsPressed())
         {
             sprite.DOLocalJump(Vector3.zero, 2, 1, 1);
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
     }
 }
